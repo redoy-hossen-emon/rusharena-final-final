6352732f76ba1a7640f071b12268de381c2e2cae
redoy <redoy.hossen.emon@gmail.com>
2026-03-13 00:09:00 +0600

    added username edit

--- utils/push.js
+++ utils/push.js
@@ -13,7 +13,7 @@ export async function initPush() {
     console.log("DEVICE TOKEN:", token.value);
 
     // Save to your database
-    fetch(`/api/save-token`, {
+    fetch(`${process.env.WEB_URL}api/save-token`, {
       method: "POST",
       body: JSON.stringify({ token: token.value }),
     });
